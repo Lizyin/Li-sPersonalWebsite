58eb7422557e4fd41ab5fe6fdabdf53a4fd05af0
Li Yin <lyin1@andrew.cmu.edu>
2017-09-18 01:55:17 -0400

    Commit everything

---
+++ PersonalWebsite2/server.js
@@ -0,0 +1,21 @@
+const express = require('express')  
+const app = express()  
+const port = 3000
+
+var path = require('path')
+app.use(express.static(path.join(__dirname, '/HTML')));
+
+app.get('/', (request, response) => {  
+  response.sendFile(__dirname +'/HTML/index.html');
+})
+
+// app.get('/twitter', (request, response) => {  
+//   response.sendFile(__dirname +'/case-study-big-data-project.html');
+// })
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})

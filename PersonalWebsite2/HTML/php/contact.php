77cf6a12c9a1e3288b8bc85971b602932094fd0d
Li Yin <lyin1@andrew.cmu.edu>
2017-09-19 20:29:48 -0400

    Add Google Map

--- PersonalWebsite2/HTML/php/contact.php
+++ PersonalWebsite2/HTML/php/contact.php
@@ -44,7 +44,7 @@ if(get_magic_quotes_gpc()) {
 // Enter the email address that you want to emails to be sent to.
 // Example $address = "joe.doe@yourdomain.com";
 
-$address = "example@example.com";
+$address = "lyin3922@gmail.com";
 
 
 // Configuration option.
